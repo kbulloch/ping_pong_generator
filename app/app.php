8f4c733fcd292ac87604f25e9852dd49108b3d6f
Kyle Bulloch & Kyle Giard-Chase <student@epicodus.com>
2015-03-09 15:05:04 -0700

    Add twig and silex

--- app/app.php
+++ app/app.php
@@ -1,3 +1,26 @@
 <?php
 
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/PingPongGenerator.php";
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('pingpong_form.twig');
+    });
+
+    $app->get("/pingpong_result", function() use ($app) {
+
+        $ping_pong_generator = new PingPongGenerator;
+        $the_answer = $ping_pong_generator->generatePingPongArray($_GET['user_input']);
+
+        return $app['twig']->render('pingpong_result.twig', array('result' => $the_answer));
+    });
+
+    return $app;
+
 ?>
