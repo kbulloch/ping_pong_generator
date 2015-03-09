d24c489d29c280afd58b8bb62604a0aafce610f4
Kyle Bulloch & Kyle Giard-Chase <student@epicodus.com>
2015-03-09 12:24:14 -0700

    Add method to build array counting up to inputted number

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -2,9 +2,23 @@
 
     class PingPongGenerator
     {
-        function generatePingPongArray()
+        function generatePingPongArray($input_number)
         {
-            
+            // take the input
+            // turn it into an array
+            //return the array
+
+            $list_of_numbers = array();
+
+            $i = 1;
+
+            while ($i <= $input_number) {
+                array_push($list_of_numbers, $i);
+                ++$i;
+            }
+
+            return $list_of_numbers;
+
         }
     }
 
