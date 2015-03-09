69772c0c7bf5e81efe39cc6d7c2b705b4f995264
Kyle Bulloch & Kyle Giard-Chase <student@epicodus.com>
2015-03-09 14:03:18 -0700

    Add testPongAtFive test

--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -21,6 +21,7 @@
         //     $this->assertEquals($correct_array_of_numbers, $result);
         // }
         // the above test opposes the below test and both cannot pass at the same time
+        // ask about this later
 
 
         function testPingAtThree()
@@ -35,6 +36,20 @@
             //Assert
             $ping_at_three_array = array(1, 2, "ping", 4, 5, "ping");
             $this->assertEquals($ping_at_three_array, $result);
+        }
+
+        function testPongAtFive()
+        {
+            //Arrange
+            $test_pong_at_five = new PingPongGenerator;
+            $input = 10;
+
+            //Act
+            $result = $test_pong_at_five->generatePingPongArray($input);
+
+            //Assert
+            $pong_at_five_array = array(1, 2, 3, 4, "pong", 6, 7, 8, 9, "pong");
+            $this->assertEquals($pong_at_five_array, $result);
         }
     }
 ?>
